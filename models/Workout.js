73aafef207ac8393c62ac70df2b89a96648f157d
Ryan Conran <ryan.conran2@gmail.com>
2021-02-11 14:32:04 -0600

    Added current date to the index page.  installed packages and ignore file added

--- models/Workout.js
+++ models/Workout.js
@@ -5,7 +5,15 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
     exercises: [
         {type: mongoose.Schema.Types.ObjectId, ref: 'Exercise', require: false}
-    ]
+    ],
+    day: {
+        type: Date,
+        default: () => new Date(),
+      },
+    duration: {
+        type: Number,
+        required: true,
+    }
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
